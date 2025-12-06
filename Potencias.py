01a9633f26babef363f7dcb9e5033567a79901a8
JosueRosario <josuerosario425@gmail.com>
2025-12-05 21:08:17 -0400

    agrega script para calcular potencias.

---
+++ Potencias.py
@@ -0,0 +1,4 @@
+print("este es el inicio de la potencia")
+
+for i in range(1, 6):
+    print(f"{i}^2={i**2}") 
--- contador.py
+++ contador.py
@@ -7,3 +7,5 @@ print("Hola!, este es el contador.")
 
 for i in range(1, 6):
     print("numero: {i}")
+    
+
